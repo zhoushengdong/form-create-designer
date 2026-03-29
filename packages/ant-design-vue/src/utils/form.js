96e14005f309fcc594ea9bff63cf9e4f3fd4e99b
zsd <www.124780540@qq.com>
2026-03-29 21:16:00 +0800

    chore: 移除 ant-design-vue 组件库和 vant 组件库

--- packages/ant-design-vue/src/utils/form.js
+++
@@ -1,9 +0,0 @@
-import formCreate from '@form-create/ant-design-vue';
-
-const viewForm = formCreate;
-
-const designerForm = formCreate.factory();
-
-export default viewForm;
-
-export {designerForm};
